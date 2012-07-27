6a09052ea46174d6df142b7861f90966d564a9ff
zhubicen <zhubicen@gmail.com>
2012-07-27 20:57:31 +0800

    refactor

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	. "github.com/ZhuBicen/go-winapi" 
 	//"github.com/AllenDang/w32"
@@ -10,11 +11,23 @@ import (
 	"unsafe"
 )
 
-func main() {
+var usrEnv map[string]string
+var sysEnv map[string]string
+
+type EnvType int
+
+func ReadEnvMap(etype EnvType) (map[string]string, error) {
 	var  hkey HKEY
-	RegOpenKeyEx(HKEY_CURRENT_USER, syscall.StringToUTF16Ptr(`Environment`), 0, KEY_READ, &hkey)
-	//RegOpenKeyEx(HKEY_LOCAL_MACHINE, syscall.StringToUTF16Ptr(`SYSTEM\CurrentControlSet\Control\Session Manager\Environment`), 0, KEY_READ, &hkey)
-	for i := 0; i < 10; i++ {
+	envMap := make(map[string]string)
+	if etype == 0 {
+		RegOpenKeyEx(HKEY_CURRENT_USER, syscall.StringToUTF16Ptr(`Environment`), 0, KEY_READ, &hkey)
+	}else {
+		RegOpenKeyEx(HKEY_LOCAL_MACHINE, 
+			syscall.StringToUTF16Ptr(`SYSTEM\CurrentControlSet\Control\Session Manager\Environment`), 
+			0, KEY_READ, &hkey)	
+	}
+
+	for i := 0; ; i++ {
 
 		//http://msdn.microsoft.com/en-us/library/windows/desktop/ms724872(v=vs.85).aspx
 		var valueLen uint32 = 256
@@ -30,10 +43,18 @@ func main() {
 		dataBuffer := make([]uint16, dataLen/2 + 1)
 
 		if ERROR_SUCCESS != RegQueryValueEx(hkey, &valueBuffer[0], nil, &dataType, (*byte)(unsafe.Pointer(&dataBuffer[0])), &dataLen){
-			fmt.Println("ERROR2")
+			return nil, errors.New("ERROR2")
 		}
-		fmt.Println(syscall.UTF16ToString(valueBuffer),"=",  syscall.UTF16ToString(dataBuffer))
-		//fmt.Println(syscall.UTF16ToString(valueBuffer), "=", syscall.UTF16ToString(dataBuffer), dataType, valueLen, dataLen)
-	}
+		envMap[syscall.UTF16ToString(valueBuffer)] = syscall.UTF16ToString(dataBuffer)
 
+	}
+	return envMap, nil
+}
+func main() {
+	if usrMap, err := ReadEnvMap(0); err == nil {
+		fmt.Println(usrMap)
+	}
+	if sysMap, err := ReadEnvMap(1); err == nil {
+		fmt.Println(sysMap)
+	}
 }
