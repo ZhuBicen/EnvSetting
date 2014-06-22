04a4c3e27292089c881393ebc087dc8f2c99efab
bzhu <zhubicen@gmail.com>
2014-06-22 19:39:39 +0800

    TODO: change register

--- env/model.go
+++ env/model.go
@@ -2,7 +2,6 @@ package env
 
 import (
 	"github.com/lxn/walk"
-	"log"
 	"sort"
 )
 
@@ -13,14 +12,14 @@ type Variable struct {
 	Value string
 }
 
-type EnvModel struct {
+type Model struct {
 	envType EnvType
 	walk.TableModelBase
 	items []*Variable
 }
 
-func NewModel(env EnvType) *EnvModel {
-	m := new(EnvModel)
+func NewModel(env EnvType) *Model {
+	m := new(Model)
 	m.envType = env
 
 	m.ResetRows()
@@ -30,12 +29,12 @@ func NewModel(env EnvType) *EnvModel {
 
 // Called by the TableView from SetModel and every time the model publishes a
 // RowsReset event.
-func (m *EnvModel) RowCount() int {
+func (m *Model) RowCount() int {
 	return len(m.items)
 }
 
 // Called by the TableView when it needs the text to display for a given cell.
-func (m *EnvModel) Value(row, col int) interface{} {
+func (m *Model) Value(row, col int) interface{} {
 	item := m.items[row]
 
 	switch col {
@@ -50,67 +49,75 @@ func (m *EnvModel) Value(row, col int) interface{} {
 }
 
 // Called by the TableView to retrieve if a given row is checked.
-func (m *EnvModel) Checked(row int) bool {
+func (m *Model) Checked(row int) bool {
 	return false
 }
 
-func (m *EnvModel) Len() int {
+func (m *Model) Len() int {
 	return len(m.items)
 }
 
 // Called by the TableView to retrieve an item image.
-func (m *EnvModel) Image(row int) interface{} {
+func (m *Model) Image(row int) interface{} {
 	return nil
 }
-func (m *EnvModel) Less(i, j int) bool {
+func (m *Model) Less(i, j int) bool {
 	return m.items[i].Name < m.items[j].Name
 }
 
-func (m *EnvModel) Swap(i, j int) {
+func (m *Model) Swap(i, j int) {
 	m.items[i], m.items[j] = m.items[j], m.items[i]
 }
-func (m *EnvModel) ResetRows() {
+func (m *Model) ResetRows() {
 	if usrEnv, err := ReadVariables(m.envType); err != nil {
 		panic("Fail to read the user env")
 	} else {
 		m.items = make([]*Variable, 0)
 		for k, v := range usrEnv {
 			m.items = append(m.items, &Variable{k, v})
-			log.Println(k, "=>", v)
 		}
 	}
 	sort.Sort(m)
 	// Notify TableView and other interested parties about the reset.
 	m.PublishRowsReset()
 }
-func (m *EnvModel) AddVariable(name string, value string) bool {
+func (m *Model) AddVariable(name string, value string) bool {
 	if e, _ := m.exists(name); e {
 		return false
 	}
 	m.items = append(m.items, &Variable{name, value})
+	sort.Sort(m)
+	// Notify TableView and other interested parties about the reset.
+	m.PublishRowsReset()
 	return true
 }
-func (m *EnvModel) GetVariable(index int) *Variable {
+func (m *Model) GetVariable(index int) *Variable {
 	return m.items[index]
 }
-func (m *EnvModel) EditVariable(name string, value string) bool {
+func (m *Model) EditVariable(name string, value string) bool {
 	if e, v := m.exists(name); e {
 		v.Value = value
+		sort.Sort(m)
+		// Notify TableView and other interested parties about the reset.
+		m.PublishRowsReset()
 		return true
 	}
 	return false
 }
 
-func (m *EnvModel) DeleteVariable(name string) bool {
+func (m *Model) DeleteVariable(name string) bool {
 	for i, variable := range m.items {
 		if variable.Name == name {
 			m.items = append(m.items[:i], m.items[i+1:]...)
+			sort.Sort(m)
+			// Notify TableView and other interested parties about the reset.
+			m.PublishRowsReset()
 			return true
 		}
 	}
 	return false
 }
-func (m *EnvModel) exists(name string) (bool, *Variable) {
+func (m *Model) exists(name string) (bool, *Variable) {
 	for i, variable := range m.items {
 		if variable.Name == name {
 			return true, m.items[i]
--- main.go
+++ main.go
@@ -4,10 +4,39 @@ import (
 	"./env"
 	"github.com/lxn/walk"
 	. "github.com/lxn/walk/declarative"
+	"github.com/lxn/win"
 	"log"
 	"os/user"
 )
 
+func NewVariable(mw *walk.MainWindow, m *env.Model) {
+	if ret, name, value := ShowDialog(mw, "", ""); ret == 0 {
+		log.Println("You will creating var name = ", name, ", value = ", value)
+		if !m.AddVariable(name, value) {
+			walk.MsgBox(mw, "Error", "The variable has already existed.", walk.MsgBoxOK)
+		}
+	}
+}
+func EditVariable(mw *walk.MainWindow, m *env.Model, name string, value string) {
+	if ret, newName, newValue := ShowDialog(mw, name, value); ret == 0 {
+		if newName != name {
+			walk.MsgBox(mw, "Error", "You can't change the variable name when editing.", walk.MsgBoxOK)
+			return
+		}
+		if !m.EditVariable(name, newValue) {
+			walk.MsgBox(mw, "Error", "Please ensure the variable has already existed.", walk.MsgBoxOK)
+			return
+		}
+	}
+}
+
+func DeleteVariable(mw *walk.MainWindow, m *env.Model, name string) {
+	if win.IDYES == walk.MsgBox(mw, "Information", "You want to delete variable "+name, walk.MsgBoxYesNo) {
+		if !m.DeleteVariable(name) {
+			walk.MsgBox(mw, "Error", "Please ensure the variable has already existed.", walk.MsgBoxOK)
+		}
+	}
+}
 func main() {
 	font := Font{
 		Family:    "Times New Roman",
@@ -45,7 +74,9 @@ func main() {
 								Model:               usrModel,
 								OnItemActivated: func() {
 									index := usrTableView.CurrentIndex()
-									ShowDialog(mw, usrModel.GetVariable(index).Name, usrModel.GetVariable(index).Value)
+									if index != -1 {
+										EditVariable(mw, usrModel, usrModel.GetVariable(index).Name, usrModel.GetVariable(index).Value)
+									}
 								},
 							},
 							Composite{
@@ -55,20 +86,26 @@ func main() {
 									PushButton{
 										Text: "New...",
 										OnClicked: func() {
-											if ret, name, value := ShowDialog(mw, "", ""); ret == 0 {
-												log.Println("You will creating var name = ", name, ", value = ", value)
-											}
+											NewVariable(mw, usrModel)
 										},
 									},
 									PushButton{
 										Text: "Edit...",
 										OnClicked: func() {
 											index := usrTableView.CurrentIndex()
-											ShowDialog(mw, usrModel.GetVariable(index).Name, usrModel.GetVariable(index).Value)
+											if index != -1 {
+												EditVariable(mw, usrModel, usrModel.GetVariable(index).Name, usrModel.GetVariable(index).Value)
+											}
 										},
 									},
 									PushButton{
 										Text: "Delete",
+										OnClicked: func() {
+											index := usrTableView.CurrentIndex()
+											if index != -1 {
+												DeleteVariable(mw, usrModel, usrModel.GetVariable(index).Name)
+											}
+										},
 									},
 								},
 							},
@@ -91,7 +128,9 @@ func main() {
 								Model:               sysModel,
 								OnItemActivated: func() {
 									index := sysTableView.CurrentIndex()
-									ShowDialog(mw, sysModel.GetVariable(index).Name, sysModel.GetVariable(index).Value)
+									if index != -1 {
+										EditVariable(mw, sysModel, sysModel.GetVariable(index).Name, sysModel.GetVariable(index).Value)
+									}
 								},
 							},
 							Composite{
@@ -100,16 +139,27 @@ func main() {
 									HSpacer{},
 									PushButton{
 										Text: "New...",
+										OnClicked: func() {
+											NewVariable(mw, sysModel)
+										},
 									},
 									PushButton{
 										Text: "Edit...",
 										OnClicked: func() {
-											index := usrTableView.CurrentIndex()
-											ShowDialog(mw, sysModel.GetVariable(index).Name, sysModel.GetVariable(index).Value)
+											index := sysTableView.CurrentIndex()
+											if index != -1 {
+												EditVariable(mw, sysModel, sysModel.GetVariable(index).Name, sysModel.GetVariable(index).Value)
+											}
 										},
 									},
 									PushButton{
 										Text: "Delete",
+										OnClicked: func() {
+											index := sysTableView.CurrentIndex()
+											if index != -1 {
+												DeleteVariable(mw, sysModel, sysModel.GetVariable(index).Name)
+											}
+										},
 									},
 								},
 							},
